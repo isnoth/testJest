9f3df5795d45eb3df82fbbe388e89e9e2adc84b8
root <root@vultr.guest>
2016-03-25 01:15:34 +0000

    webpack

--- demo1/src/index.js
+++ demo1/src/index.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import ChecboxWithLabel from "./CheckboxWithLabel"
+
+ReactDOM.render(
+  <div>
+    <ChecboxWithLabel />
+    <h1>Hello, world!</h1>,
+  </div>,
+  document.querySelector('#wrapper')
+);
